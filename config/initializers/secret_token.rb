93908acc4943ff78ca5b068df21b6a1bcb0bcd8f
Giola-celeste Fritz <gioia-celeste.fritz@project-a.com>
2014-05-21 10:33:49 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PmApp::Application.config.secret_key_base = 'f6210f8cc4fb3630b1f7d4112206cdede6e128014f60422d615004ce028b02906c94429c6b305a60215840cb4e6f47c8c93e93b41633fcd72fba1a151c984f55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PmApp::Application.config.session_store :cookie_store, key: '_pm_app_session'
