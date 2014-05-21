ba1e8e2f404d39ef1fbe80377d3ac42f81e0ccbd
Giola-celeste Fritz <gioia-celeste.fritz@project-a.com>
2014-05-21 17:54:52 +0200

    Adding bootstrap and full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,50 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+	subject { page }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('PM')
-    end
+	shared_examples_for "all static pages" do
+    	it { should have_selector('h1', text: heading) }
+    	it { should have_title(full_title(page_title)) }
+  	end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial PM App | Home")
-    end
-  end
+	describe "Home page" do
+	    before { visit root_path }
+	    let(:heading)    { 'PM App' }
+	    let(:page_title) { '' }
 
-  describe "Help page" do
+	    it_should_behave_like "all static pages"
+	    it { should_not have_title('| Home') }
+	end
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+	describe "Help page" do
+	    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial PM App | Help")
-    end
-  end
+	    let(:heading)    { 'Help' }
+	    let(:page_title) { 'Help' }
 
-  describe "About page" do
+	    it_should_behave_like "all static pages"
+	end
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
-    end
+	describe "About page" do
+		before { visit about_path }
 
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial PM App | About Me")
-    end
-  end
+		let (:heading)		{ 'About' }
+		let (:page_title)	{ 'About' }
+
+		it_should_behave_like "all static pages"
+	end
+
+	describe "Static pages" do 
+	  	it "should have the right links on the layout" do
+		    visit root_path
+		    click_link "About"
+		    expect(page).to have_title(full_title('About'))
+		    click_link "Help"
+		    expect(page).to have_title(full_title('Help'))
+		    click_link "Home"
+		    click_link "Sign up now!"
+		    expect(page).to have_title(full_title('Home'))
+		end
+	end
 end
